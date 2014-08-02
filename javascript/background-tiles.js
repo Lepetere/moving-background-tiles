baaed1edbd0be3181ef03ed47e988d53160f3d4a
Peter Fessel <peter.fessel@live.de>
2014-08-02 14:33:17 +0200

    tile insertion works: several div elements, which are supposed to work as the tiles, are inserted into the html document

--- javascript/background-tiles.js
+++ javascript/background-tiles.js
@@ -1,4 +1,4 @@
-$.ready(function () {
+$().ready(function () {
 
 	var BACKGROUNDSQUARE_MAX_WIDTH = 120;
 	var BACKGROUNDSQUARE_MAX_HEIGHT = 120;
@@ -14,18 +14,25 @@ $.ready(function () {
 		var backgroundSquareWidth = windowWidth / horizontalNumberOfSquares;
 		var backgroundSquareHeight = windowHeight / verticalNumberOfSquares;
 
-		var squareContainer = $('body').append('div').addClass('background-tile-container');
+		$('body').append('<div class="background-tile-container"></div>');
+		var squareContainer = $('.background-tile-container').first();
 		// add the appropriate number of squares to fill the background
 		for ( i = 0; i < horizontalNumberOfSquares * verticalNumberOfSquares; i++ ) {
-			var square = $(squareContainer).append('div').addClass('background-tile');
-			square.style('width', backgroundSquareWidth);
-			square.style('height', backgroundSquareHeight);
+			var square = $(squareContainer).append('<div class="background-tile">hallo</div>').children('.background-tile').last();
+			$(square).css('width', backgroundSquareWidth);
+			$(square).css('height', backgroundSquareHeight);
 		}
 	};
+
+	var alterBackgroundPosition = function (event) {
+        var event = event || window.event; // IE-ism
+        console.log("x mouse potition: " + event.clientX + "; y mouse potition: " + event.clientY);
+    };
 
 	// execute makeBackgroundSquares to add the squares to the background and assign it to window.onresize
 	makeBackgroundSquares();
 	window.onresize = makeBackgroundSquares;
 
 	// add mouse move event handler to alter background of squares
+	window.onmousemove = alterBackgroundPosition;
 });
